7a8f6e8b8dece8d12b3ccf2d3b6f8369fea05c8b
Ramjan Ansari <ramjanansari@gmail.com>
2025-06-12 11:16:45 +0530

    add file

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const mongodb_url = process.env.MONGODB_URL;
-
 async function connectDb() {
   try {
-    await mongoose.connect(`${mongodb_url}/bg-removar`);
+    await mongoose.connect(`${process.env.MONGODB_URL}/bg-removar`);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error("MongoDB connection error:",error);
   }
 }
+
 
 export default connectDb;
